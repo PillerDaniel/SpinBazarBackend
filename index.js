8e0ed65c3808e64efd1c9b0bb3df5a7ac45abadf
akosszanto <szantoakos987@gmail.com>
2025-03-23 15:44:18 +0100

    credentials

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const corsOptions = {
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
