0979218f9be8cb341a888e1c1b2c9458e5c1c807
PillerDaniel <pdanitwitch@gmail.com>
2025-03-24 18:20:49 +0100

    getusers

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,26 @@
 const History = require("../models/History");
+const User = require("../models/User");
 const adminMiddleware = require("../middleware/adminMiddleware");
 const express = require("express");
 
 const router = express.Router();
 
-router.get("/test", adminMiddleware, async (req, res) => {
-  return res.json({ message: "Hali" });
+//get users
+// admin/getusers
+
+router.get("/getusers", adminMiddleware, async (req, res) => {
+  try {
+    const users = await User.find({}).select("-password -__v");
+
+    if (!users) {
+      return res.status(400).json({ message: "No users found." });
+    }
+
+    return res.status(200).json({ users: users });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 module.exports = router;
