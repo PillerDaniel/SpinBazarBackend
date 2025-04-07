e68490bf18e763d77dad5182e126bea6779002aa
PillerDaniel <pdanitwitch@gmail.com>
2025-04-07 19:33:03 +0200

    user changepassword, changeemail route

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,13 @@ const User = require("../models/User");
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const authMiddlewareForSSE = require("../middleware/authMiddlewareForSSE");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
+
+// account
+// user/account
 
 router.get("/account", authMiddleware, async (req, res) => {
   try {
@@ -27,6 +32,89 @@ router.get("/account", authMiddleware, async (req, res) => {
     return res.status(500).json({ message: "Internal server error." });
   }
 });
+
+// change password
+// user/changepassword
+router.put(
+  "/changepassword",
+  [
+    //newPassword validation, same validation as in register
+    body("newPassword")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters.")
+      .matches(/[A-Z]/)
+      .withMessage("Password must contain at least one uppercase letter.")
+      .matches(/\d/)
+      .withMessage("Password must contain at least one number."),
+  ],
+  authMiddleware,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { oldPassword, newPassword } = req.body;
+    console.log("oldPassword", oldPassword);
+    console.log("newPassword", newPassword);
+
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId);
+      console.log("user", user);
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+
+      if (!isMatch) {
+        return res.status(401).json({ message: "Old password doesn't match." });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+      console.log("hashedNewPassword", hashedNewPassword);
+
+      user.password = hashedNewPassword;
+      await user.save();
+      return res
+        .status(200)
+        .json({ message: "Password changed successfully." });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  }
+);
+
+// user/changeemail
+router.put(
+  "/changeemail",
+  [body("newEmail").isEmail().withMessage("Invalid email.")],
+  authMiddleware,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { newEmail } = req.body;
+
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
+
+      user.email = newEmail;
+      await user.save();
+      return res.status(200).json({ message: "Email changed successfully." });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  }
+);
 
 //sse
 router.get("/event", authMiddlewareForSSE, async (req, res) => {
